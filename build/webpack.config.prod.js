e55be6140ab5d9c2aafcbbd936067e5bce1f5dd1
alan.byrn@ectrain.com <alan.byrn@ectrain.com>
2019-03-28 18:59:58 -0500

    Changes for production build

---
+++ build/webpack.config.prod.js
@@ -0,0 +1,75 @@
+'use strict'
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const path = require('path')
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    app: './src/main.js'
+  },
+  devServer:{
+    hot: true,
+    watchOptions:{
+        poll: true
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.vue$/,
+        exclude: /static/,
+        // use: 'vue-loader',
+        loader: 'vue-loader',
+        options: {
+          transformAssetUrls: {
+            video: ['src', 'poster'],
+            source: 'src',
+            img: 'src',
+            image: 'xlink:href',
+            'b-img': 'src',
+            'b-img-lazy': ['src', 'blank-src'],
+            'b-card': 'img-src',
+            'b-card-img': 'img-src',
+            'b-card-img-lazy': ['src', 'blank-src'],
+            'b-carousel-slide': 'img-src',
+            'b-embed': 'src'
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /static/,
+        use: [
+            'vue-style-loader',
+            'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /static/,
+        use: 'babel-loader'
+      }
+
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'index.html',
+        inkect: true
+    })
+  ]
+}
