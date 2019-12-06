24fad8d4c90da9920d72b2fff4dc11d56b0a8195
alan.byrn@ectrain.com <alan.byrn@ectrain.com>
2019-12-06 17:55:43 -0600

    change links under header-login and footer to match new domain of rapidresponseinsights.com

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,53 +1,59 @@
 <template>
-    <div>
-        <b-navbar toggleable="sm" variant="white">
-            <b-navbar-brand href="#">
-                <router-link :to="{ name: 'Home' }">
-                    <img class="ectLogo" alt="ECT logo" src="../img/ect_logo_redblk.gif">
-                </router-link>
-            </b-navbar-brand>
-            <b-navbar-toggle target="nav_collapse"/>
-            <b-collapse is-nav id="nav_collapse">
-                <b-navbar-nav>
-                    <b-nav-item-dropdown text="Features" right>
-                        <b-dropdown-item to="/features">How it Works</b-dropdown-item>                        
-                        <b-dropdown-item href="/features#collection">Collection</b-dropdown-item>
-                        <b-dropdown-item href="/features#analysis">Analysis</b-dropdown-item>
-                    </b-nav-item-dropdown>
-                    <b-nav-item-dropdown text="Plans" right>
-                        <b-dropdown-item to="/plans">Plans</b-dropdown-item>                        
-                        <b-dropdown-item href="/plans#PilotProgram">Pilot</b-dropdown-item>
-                    </b-nav-item-dropdown>
-                    <b-nav-item to="/ourteam">Our Team</b-nav-item> 
-                    <b-nav-item-dropdown text="Login" right> 
-                        <b-dropdown-item href="https://www.cibica.co" target="_blank">Collection Tool</b-dropdown-item>                        
-                        <b-dropdown-item href="https://analysis.cibica.co" target="_blank">Analysis Tool</b-dropdown-item>
-                    </b-nav-item-dropdown>
-                </b-navbar-nav>
-            </b-collapse>
-        </b-navbar>
-    </div>
+  <div>
+    <b-navbar toggleable="sm" variant="white">
+      <b-navbar-brand href="#">
+        <router-link :to="{ name: 'Home' }">
+          <img class="ectLogo" alt="ECT logo" src="../img/ect_logo_redblk.gif" />
+        </router-link>
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav_collapse" />
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item-dropdown text="Features" right>
+            <b-dropdown-item to="/features">How it Works</b-dropdown-item>
+            <b-dropdown-item href="/features#collection">Collection</b-dropdown-item>
+            <b-dropdown-item href="/features#analysis">Analysis</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-item-dropdown text="Plans" right>
+            <b-dropdown-item to="/plans">Plans</b-dropdown-item>
+            <b-dropdown-item href="/plans#PilotProgram">Pilot</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-item to="/ourteam">Our Team</b-nav-item>
+          <b-nav-item-dropdown text="Login" right>
+            <b-dropdown-item
+              href="https://insights.rapidresponseinsights.com"
+              target="_blank"
+            >Collection Tool</b-dropdown-item>
+            <b-dropdown-item
+              href="https://analysis.rapidresponseinsights.com"
+              target="_blank"
+            >Analysis Tool</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 <script>
 export default {
-  name: 'Header'
-}
+  name: "Header"
+};
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    .ectLogo{
-        width:120px;
-        height:120px;
-    }
-    .ectMenu{
-        font-size:24px;
-        font-weight:600;
-    }
-    #nav_collapse {
-        font-size:24px;
-        font-weight:600;
-    }
-    a {
-        color:#6D6E70;
-    }
+.ectLogo {
+  width: 120px;
+  height: 120px;
+}
+.ectMenu {
+  font-size: 24px;
+  font-weight: 600;
+}
+#nav_collapse {
+  font-size: 24px;
+  font-weight: 600;
+}
+a {
+  color: #6d6e70;
+}
 </style>
--- src/components/RnRFooter.vue
+++ src/components/RnRFooter.vue
@@ -1,35 +1,67 @@
 <template>
-    <div class="RnRFooter container">
-        <hr>
-        <div class="row text-left">
-            <div class="col-9 offset-3"> <h3>For the latest, follow us on LinkedIn <a href="https://www.linkedin.com/company/ect-us"><img id="footerLinkedIn" src="../img/redlinkedin612x612.png" /></a></h3></div>
-        </div>
-        <div class="row text-left">
-            <div class="col-9 offset-3"><h3>Schedule your Free Consult<img id="footerPhone" src="../img/redphone256x256.png"/></h3></div>
-        </div>
-        <div class="row text-left">
-            <div class="col-9 offset-3 "><span >Contact Us</span> <b class="Black">(888) 843-5691</b> | <a class="ECTEmail" href="mailto:solutions@ecttechnologies.com?subject=Mail us some info on Rapid Response Insights">solutions@ecttechnologies.com</a></div>
-        </div>
-        <div class="row text-left">
-            <div class="col-9 offset-3">
-                <nav id="footerNavLinks">
-                        <router-link to="/"><span><a href="#">Home</a></span></router-link>
-                        |
-                        <router-link to="/features"><span><a href="#">Features</a></span></router-link>
-                        |                   
-                        <router-link to="/plans"><span><a href="#">Plans</a></span></router-link>
-                        |                              
-                        <router-link to="/ourteam"><span><a href="#">Our Team</a></span></router-link>
-                        |                   
-                        <span><a href="https://analysis.cibica.co" target="_blank">Login</a></span>       
-                   
-                </nav>
-            </div>
-        </div>        
-        <div class="row text-left">
-            <div class="col-9 offset-3"> &#169; ECT Technologies LLC. All Rights Reserved</div>
-        </div>
-        <!-- <div class="row text-left">
+  <div class="RnRFooter container">
+    <hr />
+    <div class="row text-left">
+      <div class="col-9 offset-3">
+        <h3>
+          For the latest, follow us on LinkedIn
+          <a href="https://www.linkedin.com/company/ect-us">
+            <img id="footerLinkedIn" src="../img/redlinkedin612x612.png" />
+          </a>
+        </h3>
+      </div>
+    </div>
+    <div class="row text-left">
+      <div class="col-9 offset-3">
+        <h3>
+          Schedule your Free Consult
+          <img id="footerPhone" src="../img/redphone256x256.png" />
+        </h3>
+      </div>
+    </div>
+    <div class="row text-left">
+      <div class="col-9 offset-3">
+        <span>Contact Us</span>
+        <b class="Black">(888) 843-5691</b> |
+        <a
+          class="ECTEmail"
+          href="mailto:solutions@ecttechnologies.com?subject=Mail us some info on Rapid Response Insights"
+        >solutions@ecttechnologies.com</a>
+      </div>
+    </div>
+    <div class="row text-left">
+      <div class="col-9 offset-3">
+        <nav id="footerNavLinks">
+          <router-link to="/">
+            <span>
+              <a href="#">Home</a>
+            </span>
+          </router-link>|
+          <router-link to="/features">
+            <span>
+              <a href="#">Features</a>
+            </span>
+          </router-link>|
+          <router-link to="/plans">
+            <span>
+              <a href="#">Plans</a>
+            </span>
+          </router-link>|
+          <router-link to="/ourteam">
+            <span>
+              <a href="#">Our Team</a>
+            </span>
+          </router-link>|
+          <span>
+            <a href="https://analysis.rapidresponseinsights.com" target="_blank">Login</a>
+          </span>
+        </nav>
+      </div>
+    </div>
+    <div class="row text-left">
+      <div class="col-9 offset-3">&#169; ECT Technologies LLC. All Rights Reserved</div>
+    </div>
+    <!-- <div class="row text-left">
             <div class="col-9 offset-3">
             <p> 
                 <nav>
@@ -41,71 +73,65 @@
                 </nav>
             </p>
             </div>
-        </div> -->
-    </div>
+    </div>-->
+  </div>
 </template>
 
 <script>
-
 export default {
-    name: 'RnRFooter',
-    props: {
-       
-    },
-    components: {
-       
-    }
-}
+  name: "RnRFooter",
+  props: {},
+  components: {}
+};
 </script>
 <style scoped>
-    hr {
-        margin: 6rem 0rem;
-    }
-    .RnRFooter {
-        margin: 4rem 0rem; 
-        font-size: 18px;        
-        color: gray;
-        text-align: center;
-        font-family: 'Indie Flower','Amatic SC','Handlee','Damion', cursive;
-    }
-    .RnRFooter a {
-        color: gray;
-    }
-    
-    .Black {
-        color:black;
-    }
+hr {
+  margin: 6rem 0rem;
+}
+.RnRFooter {
+  margin: 4rem 0rem;
+  font-size: 18px;
+  color: gray;
+  text-align: center;
+  font-family: "Indie Flower", "Amatic SC", "Handlee", "Damion", cursive;
+}
+.RnRFooter a {
+  color: gray;
+}
 
-    #footerNavLinks ul {    
-    list-style:none;
-    padding-left: 0px;
-    /* text-align: left; */
-    /* vertical-align: text-top; */
-    }
+.Black {
+  color: black;
+}
 
-    #footerNavLinks ul li {    
-    /* width:auto;    */
-    cursor:pointer;   
-    display:inline-block;
-    
-    }
+#footerNavLinks ul {
+  list-style: none;
+  padding-left: 0px;
+  /* text-align: left; */
+  /* vertical-align: text-top; */
+}
 
-    #footerNavLinks ul li a{
-    /* padding:5px 15px;     */
-    
-    color: #808080;
-    display:inline-block;
-    text-decoration:none;
-    }
+#footerNavLinks ul li {
+  /* width:auto;    */
+  cursor: pointer;
+  display: inline-block;
+}
 
-    #footerLinkedIn{      
-        width: 30px;
-        height: 30px;
-        margin-left: .4rem;
-    }
-   #footerPhone{      
-        width: 30px;
-        height: 30px;
-        margin-left: .4rem;
-    }
+#footerNavLinks ul li a {
+  /* padding:5px 15px;     */
+
+  color: #808080;
+  display: inline-block;
+  text-decoration: none;
+}
+
+#footerLinkedIn {
+  width: 30px;
+  height: 30px;
+  margin-left: 0.4rem;
+}
+#footerPhone {
+  width: 30px;
+  height: 30px;
+  margin-left: 0.4rem;
+}
 </style>
